e32dd0e226dec0cd595572f03e3b35e99042d065
Hane Liu <hane0131@gmail.com>
2016-01-16 16:20:23 +0800

    define a class Classifier

--- classification.py
+++ classification.py
@@ -2,18 +2,22 @@ import csv
 import numpy as np
 
 
-def load_data(filename):
-    with open(filename, 'rt') as fin:
-        cin = csv.reader(fin)
-        data = [row for row in cin]
+class Classifier:
+    def __init__(self, training_file):
+        self.training_file = training_file
+        self.origin_data = []
+        self.features = []
+        self.feature_name = []
+        self.labels = []
+        self.label_name = ""
 
-    return data
+    def load_data(self):
+        with open(self.training_file, 'rt') as fin:
+            cin = csv.reader(fin)
+            self.origin_data = [row for row in cin]
 
-
-def separate_data(origin_data):
-    label_name = np.array(origin_data[0][-1])
-    feature_name = np.array(origin_data[0][0:-1])
-    labels = np.array([i[-1] for i in origin_data[1:-1]])
-    features = np.array([row[0:-1] for row in origin_data[1:-1]], dtype='float')
-
-    return label_name, feature_name, labels, features
+    def separate_data(self):
+        self.label_name = np.array(self.origin_data[0][-1])
+        self.feature_name = np.array(self.origin_data[0][0:-1])
+        self.labels = np.array([i[-1] for i in self.origin_data[1:-1]])
+        self.features = np.array([row[0:-1] for row in self.origin_data[1:-1]], dtype='float')
