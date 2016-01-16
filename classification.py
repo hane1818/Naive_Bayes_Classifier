49818ba3b8b45e09fc18f867028b2041c2fef13b
Hane Liu <hane0131@gmail.com>
2016-01-16 19:17:52 +0800

    complete train model method & not use numpy array

--- classification.py
+++ classification.py
@@ -1,5 +1,5 @@
 import csv
-import numpy as np
+import math
 
 
 class Classifier:
@@ -9,13 +9,14 @@ class Classifier:
         self.feature_name = []
         self.labels = []
         self.label_name = ""
-        self.model = []
-        self.test_model = []    # use for training more & more times
+        self.class_label = set()
+        self.model = {}
+        self.test_model = {}    # use for training more & more times
 
     def load_file(self, filename):
         with open(filename, 'rt') as fin:
             cin = csv.reader(fin)
-            self.origin_data = np.array([row for row in cin])
+            self.origin_data =[row for row in cin]
         return self
 
     def load_data(self, datasets):
@@ -23,13 +24,51 @@ class Classifier:
 
     def separate_data(self):
         try:
-            self.label_name = np.array(self.origin_data[0][-1])
-            self.feature_name = np.array(self.origin_data[0][0:-1])
-            self.labels = np.array([i[-1] for i in self.origin_data[1:-1]])
-            self.features = np.array([row[0:-1] for row in self.origin_data[1:-1]], dtype='float')
+            self.label_name = self.origin_data[0][-1]
+            self.feature_name = self.origin_data[0][0:-1]
+            self.labels = [i[-1] for i in self.origin_data[1:-1]]
+            self.features = [row[0:-1] for row in self.origin_data[1:-1]]
+            for i, data in enumerate(self.features):
+                for j, d in enumerate(data):
+                    self.features[i][j] = float(d)
+            self.class_label = set(self.labels)
         except IndexError:
             raise ValueError("no data in classifier, use load_data() to load a data set.")
         return self
 
     def train(self):
-        pass
+        # first add labels in model
+        data_model = {}
+        for i in self.class_label:
+            data_model[i] = []
+
+        # second separate data by label
+        for i, data in enumerate(self.features):
+            data_model[self.labels[i]].append(data)
+        # calculate every label's probability
+        for key in self.class_label:
+            data_model[key] = (len(data_model[key])/len(self.class_label), data_model[key])
+
+        # calculate every feature's probability
+        def mean(numbers):
+            return sum(numbers)/len(numbers)
+
+        def stdev(numbers):
+            avg = mean(numbers)
+            variance = sum([pow(x-avg, 2) for x in numbers])/float(len(numbers)-1)
+            return math.sqrt(variance)
+
+        model = {}
+        for label in self.class_label:
+            model[label] = []
+            for i in range(len(self.feature_name)):
+                model[label].append([data[i] for data in data_model[label][1]])
+                model[label][i] = (mean(model[label][i]), stdev(model[label][i]))
+            model[label] = (data_model[label][0], model[label])
+
+        if self.model == {}:
+            self.model = model
+        else:
+            self.test_model = model
+
+        return self
