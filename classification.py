295399b81e803a6736146af30b1b1185eaedec52
Hane Liu <hane0131@gmail.com>
2016-01-16 16:11:16 +0800

    add method to separate data

--- classification.py
+++ classification.py
@@ -1,4 +1,5 @@
 import csv
+import numpy as np
 
 
 def load_data(filename):
@@ -7,3 +8,12 @@ def load_data(filename):
         data = [row for row in cin]
 
     return data
+
+
+def separate_data(origin_data):
+    label_name = np.array(origin_data[0][-1])
+    feature_name = np.array(origin_data[0][0:-1])
+    labels = np.array([i[-1] for i in origin_data[1:-1]])
+    features = np.array([row[0:-1] for row in origin_data[1:-1]], dtype='float')
+
+    return label_name, feature_name, labels, features
