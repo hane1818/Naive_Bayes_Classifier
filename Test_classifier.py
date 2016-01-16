ee4d3387c5ddb1bd9cd7d6f24b53e4ee6885555e
Hane Liu <hane0131@gmail.com>
2016-01-16 16:31:30 +0800

    test for load data

--- Test_classifier.py
+++ Test_classifier.py
@@ -4,4 +4,11 @@ import classification
 
 class TestClassifier(unittest.TestCase):
     def setUp(self):
-        self.classifier = classification.Classifier('only_data.csv')
+        self.classifier = classification.Classifier()
+
+    def test_load_data(self):
+        self.classifier.load_data('only_data.csv')
+        print(self.classifier.origin_data)
+
+if __name__ == '__main__':
+    unittest.main()
